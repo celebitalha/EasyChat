b51641b8cb11d17015b93ba7dc10e38dfcc708b2
celebitalha <155588819+celebitalha@users.noreply.github.com>
2025-10-29 00:31:58 +0100

    Initial Commit

---
+++ EasyChatTests/EasyChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyChatTests.swift
+//  EasyChatTests
+//
+//  Created by Talha Çelebi on 29.10.2025.
+//
+
+import Testing
+@testable import EasyChat
+
+struct EasyChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
